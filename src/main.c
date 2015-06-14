7b8e8417e8923ace768ba78e9a14b0c2596c7e7f
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-06-14 18:00:45 +0200

    wip

--- src/main.c
+++ src/main.c
@@ -5,28 +5,23 @@
 int main(void)
 {
 	HAL_Init();
-
 	SystemClock_Config();
-
-	ili9340_init(3, 0);
-	ili9340_fill_color(ILI9340_BLUE);
-
-	//for(int i = 0; i < 10; ++i)
-	//	ili9340_putc('0' + i, 50, 100 - 6 * i);
-
-	//ili9340_puts("HELLO WORLD\nTEST 123\nTEST TEST");
-
 	BSP_LED_Init(LED3);
 	BSP_LED_Init(LED4);
 	BSP_LED_Init(LED5);
 	BSP_LED_Init(LED6);
+
+	ili9340_init(3, 0);
+	ili9340_fill_color(0);
+
+	ili9340_puts("i8086 + stm32f4 = \x03\n");
 
 	while(1)
 	{
 		//ili9340_fill_color(ILI9340_BLUE);
 		//ili9340_fill_color(ILI9340_RED);
 
-		ili9340_putc('0' + rand()%10);
+		//ili9340_putc('0' + rand()%10);
 
 		//BSP_LED_Toggle(LED3);
 		//BSP_LED_Toggle(LED4);
@@ -35,12 +30,3 @@ int main(void)
 		//HAL_Delay(10);
 	}
 }
-
-#ifdef  USE_FULL_ASSERT
-void assert_failed(uint8_t* file, uint32_t line)
-{
-	while(1)
-	{
-	}
-}
-#endif
