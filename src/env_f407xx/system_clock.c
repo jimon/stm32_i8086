dcd9065765f60f04b9028d2d0107f308ac002b54
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-06-15 23:12:28 +0200

    wip

--- src/env_f407xx/system_clock.c
+++ src/env_f407xx/system_clock.c
@@ -1,7 +1,7 @@
 #include "stm32f4xx_hal.h"
 /**
   * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
+  *         The system Clock is configured as follow :
   *            System Clock source            = PLL (HSE)
   *            SYSCLK(Hz)                     = 168000000
   *            HCLK(Hz)                       = 168000000
@@ -27,8 +27,8 @@ void SystemClock_Config(void)
 	/* Enable Power Control clock */
 	__PWR_CLK_ENABLE();
 
-	/* The voltage scaling allows optimizing the power consumption when the device is 
-	clocked below the maximum system frequency, to update the voltage scaling value 
+	/* The voltage scaling allows optimizing the power consumption when the device is
+	clocked below the maximum system frequency, to update the voltage scaling value
 	regarding system frequency refer to product datasheet.  */
 	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
@@ -46,12 +46,12 @@ void SystemClock_Config(void)
 		while(1);
 	}
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
 	clocks dividers */
 	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 	if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
 	{
--- src/stm32_i8086.c
+++ src/stm32_i8086.c
@@ -34,6 +34,31 @@ RD			PD00
 BHE			PE14
 */
 
+
+extern uint32_t SystemCoreClock;
+void dwt_init(void)
+{
+	if(!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk))
+	{
+		CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+		DWT->CYCCNT = 0;
+		DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+	}
+}
+uint32_t dwt_get(void)
+{
+	return DWT->CYCCNT;
+}
+__inline uint8_t dwt_compare(int32_t tp)
+{
+	return (((int32_t)dwt_get() - tp) < 0);
+}
+void delay_us(uint32_t us)
+{
+	int32_t tp = dwt_get() + us * (SystemCoreClock / 1000000);
+	while(dwt_compare(tp)) {}
+}
+
 void i8086_wreset(bool enabled)	{HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, enabled ? GPIO_PIN_SET : GPIO_PIN_RESET);}
 void i8086_wnmi(bool enabled)	{HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, enabled ? GPIO_PIN_SET : GPIO_PIN_RESET);}
 void i8086_wintr(bool enabled)	{HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, enabled ? GPIO_PIN_SET : GPIO_PIN_RESET);}
@@ -99,20 +124,20 @@ uint32_t i8086_raddr()
 			;
 }
 
-uint32_t tick_delay = 10;
+uint32_t tick_delay = 1;
 void i8086_wclk_redge() {i8086_wclk(true);}
-void i8086_wclk_up_wait() {HAL_Delay(tick_delay);}
+void i8086_wclk_up_wait() {delay_us(tick_delay);}
 void i8086_wclk_fedge() {i8086_wclk(false);}
-void i8086_wclk_down_wait() {HAL_Delay(tick_delay * 2);}
+void i8086_wclk_down_wait() {delay_us(tick_delay * 2);}
 
 void i8086_wclk_ticks(uint32_t ticks)
 {
 	for(uint32_t i = 0; i < ticks; ++i)
 	{
+		i8086_wclk_down_wait();
 		i8086_wclk_redge();
 		i8086_wclk_up_wait();
 		i8086_wclk_fedge();
-		i8086_wclk_down_wait();
 	}
 }
 
@@ -149,9 +174,12 @@ void i8086_bus_write()
 	HAL_GPIO_Init(GPIOE, &i8086_init_bus);
 }
 
+
+
 int i8086_init()
 {
-	HAL_Delay(50);
+	HAL_Delay(100);
+	dwt_init();
 
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
@@ -193,23 +221,25 @@ int i8086_init()
 	i8086_init_in_d.Speed = GPIO_SPEED_HIGH;
 	HAL_GPIO_Init(GPIOD, &i8086_init_in_d);
 
-	i8086_wreset(true);
+	//i8086_wclk_ticks(40); // should be 4, but whatever
+
 	i8086_wnmi(false);
 	i8086_wintr(false);
 	i8086_wready(true);
 	i8086_wtest(false);
 	i8086_whold(false);
 	i8086_wclk(false);
+	i8086_wreset(true);
 
 	i8086_wclk_ticks(10); // should be 4, but whatever
 
 	i8086_wreset(false);
 
+	i8086_wclk_ticks(5);
+
 	return 0;
 }
 
-
-uint32_t latched_addr = 0;
 
 uint8_t reset_vector[] =
 {
@@ -232,14 +262,157 @@ uint8_t reset_vector[] =
 	0x0,
 };
 
+#define kprintf(format, ...) \
+{ \
+	char temp[128]; \
+	sprintf(temp, format, ##__VA_ARGS__); \
+	ili9340_puts(temp); \
+}
+
+void i8086_debug()
+{
+	char temp[128];
+	sprintf(temp, "inta%i ale%i den%i dtr%i mio%i\nwr%i hlda%i rd%i bhe%i addr0x%x\n",
+		i8086_rinta(),
+		i8086_rale(),
+		i8086_rden(),
+		i8086_rdtr(),
+		i8086_rmio(),
+		i8086_rwr(),
+		i8086_rhlda(),
+		i8086_rrd(),
+		i8086_rbhe(),
+		i8086_raddr());
+	ili9340_puts(temp);
+}
+
 void i8086_poll()
 {
+	uint8_t cycle = 1;
+
+	uint32_t latched_addr = 0;
+	bool rd = false, wd = false;
+
+	uint32_t history[16];
+	memset(history, 0, 16 * 4);
+	uint32_t history_ptr = 0;
+
+	uint32_t lool = 0;
+	uint32_t lool2 = 0;
+
 	while(true)
 	{
-		i8086_wclk_redge();
-		i8086_wclk_up_wait();
-		i8086_wclk_fedge();
-		i8086_wclk_down_wait();
+		switch(cycle)
+		{
+		case 1: // T1, wait for ale
+			i8086_bus_read();
+			i8086_wclk_down_wait();
+			i8086_wclk_redge();
+			if(i8086_rale())
+			{
+				cycle = 2;
+				latched_addr = i8086_raddr();
 
+				history[history_ptr] = latched_addr;
+				history_ptr = (history_ptr + 1) % 16;
+				kprintf("%i\n", lool);
+				lool=0;
+			}
+			else
+			{
+				lool++;
+				//kprintf("T1: no ale\n");
+				//i8086_debug();
+				//while(true){}
+			}
+			i8086_wclk_up_wait();
+			i8086_wclk_fedge();
+			break;
+		case 2: // T2, wait for RD or WD
+			i8086_bus_read();
+			i8086_wclk_down_wait();
+			i8086_wclk_redge();
+			rd = false;
+			wd = false;
+			if(!i8086_rrd())
+			{
+				cycle = 3;
+				rd = true;
+				i8086_wready(false);
+				//kprintf("T2: rd\n");
+			}
+			if(!i8086_rwr())
+			{
+				cycle = 3;
+				wd = true;
+				i8086_wready(false);
+				kprintf("T2: wd\n");
+				while(true){}
+			}
+			i8086_wclk_up_wait();
+			i8086_wclk_fedge();
+			break;
+		case 3:
+			if(rd)
+			{
+				i8086_bus_write();
+				if(latched_addr >= 0xffff0)
+				{
+					i8086_wdata8(reset_vector[latched_addr - 0xffff0]);
+				}
+				else
+				{
+					kprintf("---\n");
+					for(int i = 0; i < 16; ++i)
+					{
+						kprintf("%x\n", history[i]);
+					}
+
+					while(true){}
+				}
+				i8086_wclk_down_wait();
+				i8086_wclk_redge();
+				delay_us(100);
+				i8086_wready(true);
+				i8086_wclk_up_wait();
+				i8086_wclk_fedge();
+				cycle = 4;
+			}
+			break;
+		case 4:
+			i8086_wclk_down_wait();
+			i8086_wclk_redge();
+			i8086_wclk_up_wait();
+
+			if(rd)
+			{
+				if(i8086_rrd())
+				{
+					cycle = 1;
+					//kprintf("T4: !rd\n");
+					kprintf("T4: %i\n", lool2);
+				}
+				else
+				{
+					lool2++;
+					//kprintf("T4: ?\n");
+					//i8086_debug();
+					//while(true){}
+
+				}
+			}
+			else
+			{
+				kprintf("T4: 2 ?\n");
+				i8086_debug();
+				while(true){}
+			}
+
+
+			i8086_wclk_fedge();
+			break;
+		default:
+			break;
+		}
 	}
 }
