ba5c5630dfb6ee06c17b35b5ed3846b6b301f012
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-06-17 21:39:44 +0200

    remove stuff

--- src/main.c
+++ src/main.c
@@ -8,16 +8,10 @@ int main(void)
 {
 	HAL_Init();
 	SystemClock_Config();
-	BSP_LED_Init(LED3);
-	BSP_LED_Init(LED4);
-	BSP_LED_Init(LED5);
-	BSP_LED_Init(LED6);
 
 	ili9340_init(3, 0);
 	ili9340_fill_color(0);
 	ili9340_puts("i8086 + stm32f4 = \x03\n");
-
-	HAL_Delay(100);
 
 	if(i8086_init() != 0)
 		ili9340_puts("failed to init 8086\n");
